8f145878cc9738a751299a5b7b702d33dcc5789a
webreflection <andrea.giammarchi@gmail.com>
2023-09-06 10:42:56 +0200

    Better JSDoc TS

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const dedent = {
  * Usable both as template literal tag or just as callback for strings, removes all spaces found
  * at the very first line of code encountered while sanitizing, keeping everything else around.
  * @param {string | TemplateStringsArray} tpl either code as string or as template, when used as tag
- * @param  {...any} [values] either an empty array or the template values, when used as tag
+ * @param  {...any} values the template interpolations, when used as tag
  * @returns {string} code without undesired indentation
  */
 const codedent = (tpl, ...values) => dedent[typeof tpl](tpl, ...values);
